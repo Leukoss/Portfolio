5b8601a0fbff4f07c875cddcede8e42fdc70df37
Leukoss <lucas.saliorliange.pro@gmail.com>
2025-08-19 21:04:44 +0200

    Version 1.0.6 - Tentative 5 de dist

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/leukoss.github.io/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-
 })
