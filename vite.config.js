33a5ea7a2e7684ce1d178b52914e96154bdd4cf5
Leukoss <lucas.saliorliange.pro@gmail.com>
2025-08-19 20:50:50 +0200

    Version 1.0.4 - Tentative 3 pour le subtree

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/leukoss.github.io/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  
+
 })
